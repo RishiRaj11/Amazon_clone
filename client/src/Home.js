2d8fcecb10384075b713dc63b6c4ee2401e07f85
Rishi Raj <86799086+RishiRaj11@users.noreply.github.com>
2023-01-23 22:08:22 +0530

    updated

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt="banner"
       />
+      <div className="home_row">
       <Product
         id="12344"
         title="dddddddddw"
@@ -16,6 +17,47 @@ const Home = () => {
         rating={5}
         image="https://www.rd.com/wp-content/uploads/2021/10/book-of-night-black-ecomm-via-amazon.jpg?fit=700,700"
       />
+       <Product
+        id="12344"
+        title="dddddddddw"
+        price={1100}
+        rating={5}
+        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?%24300x400_retinamobilex2%24"
+      />
+      </div>
+      <div className="home_row">
+      <Product
+        id="12344"
+        title="dddddddddw"
+        price={1100}
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+      />
+       <Product
+        id="12344"
+        title="dddddddddw"
+        price={1100}
+        rating={5}
+        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?%24300x400_retinamobilex2%24"
+      />
+      <Product
+        id="12344"
+        title="dddddddddw"
+        price={1100}
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+      />
+      </div>
+      <div className="home_row">
+      <Product
+        id="12344"
+        title="dddddddddw"
+        price={1100}
+        rating={5}
+        image="https://www.rd.com/wp-content/uploads/2021/10/book-of-night-black-ecomm-via-amazon.jpg?fit=700,700"
+      />
+      </div>
+     
     </div>
   );
 };
--- client/src/Product.js
+++ client/src/Product.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import "./Product.css"
-const Product = ({id,title,image,price,rating}) => {
+import React from "react";
+import "./Product.css";
+const Product = ({ id, title, image, price, rating }) => {
   return (
-    <div className='product'>
-        <div className='product_info'>
+    <div className="product">
+      <div className="product_info">
         <p>{title}</p>
-        <p className='product__price'>
-            <small>RS</small>
-            <strong>{price}</strong>
+        <p className="product__price">
+          <small>RS</small>
+          <strong>{price}</strong>
         </p>
-        <div className='product__rating'>
-            {
-                Array(rating).fill().map((_)=>(
-                    <p>⭐</p>
-                ))
-            }
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
         </div>
-        </div>
-        
-        <img  src={image} alt="" />
-        <button>Add to Cart</button>
+      </div>
+
+      <img src={image} alt="" />
+      <button>Add to Cart</button>
     </div>
-  )
-}
+  );
+};
 
 export default Product;
